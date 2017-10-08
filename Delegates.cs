fda6bd8b5ac924341ed5f23ab716af514c162217
PJ Martins <pj_martins@outlook.com>
2017-10-07 22:29:43 -0700

    progress

---
+++ Delegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaJaMa.Common
+{
+	public enum YesYesToAllNo
+	{
+		Yes,
+		YesToAll,
+		No
+	}
+
+	public class DialogEventArgs
+	{
+		public DialogEventArgs(string message)
+		{
+			Message = message;
+		}
+
+		public string Message { get; private set; }
+		public YesYesToAllNo Result { get; set; }
+	}
+
+	public delegate void DialogEventHandler(object sender, DialogEventArgs e);
+}
